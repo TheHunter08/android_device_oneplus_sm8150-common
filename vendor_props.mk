597484511021f7575270bab1397eeda22a54fc11
Anton Hansson <hansson@google.com>
2019-01-18 14:10:45 +0000

    guacamoleb: Use TARGET_SCREEN_DENSITY
    This variable is used to auto-populate the ro.sf.lcd_density as
    well as selecting density bucket for resources in recovery.
    
    Bug: 118676832
    Test: adb shell getprop ro.sf.lcd_density
    Test: boot into recovery
    Change-Id: Ib383d18b61e7a2058a69ae26136aa053fdba4df2

--- vendor_props.mk
+++ vendor_props.mk
@@ -133,7 +133,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_null_display=0 \
     vendor.display.enable_optimize_refresh=1 
 
-
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
