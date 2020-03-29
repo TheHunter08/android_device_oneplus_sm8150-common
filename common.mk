cfa72e2d0654997545be68690fb818582453d0f8
TheHunter08 <ismael.angeles.c@live.com>
2020-03-29 18:45:45 +0200

    Configure vendor Guacamoleb

--- common.mk
+++ common.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/oneplus/guacamoleb/guacamoleb-vendor.mk)
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_props.mk
@@ -24,6 +24,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-havoc/packages/apps/Snap
+
 
 # A/B
 AB_OTA_UPDATER := true
